6ef8b05fd731806a00374e07c380f8b7da7c1a17
umar006 <umaralfaruq597@gmail.com>
2024-06-07 14:34:54 +0700

    feat: driver instance from static method

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -2,6 +2,7 @@ import * as bcrypt from 'bcrypt';
 import { InferSelectModel } from 'drizzle-orm';
 import { pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { nanoid } from 'nanoid';
+import { RegisterDriverDto } from './register-driver.dto';
 import { RegisterUserDto } from './register-user.dto';
 
 export const users = pgTable('users', {
@@ -26,6 +27,30 @@ export class User implements InferSelectModel<typeof users> {
     user.username = dto.username;
     user.password = dto.password;
     user.role = 'user';
+
+    return user;
+  }
+
+  async encryptPassword(): Promise<void> {
+    const hashedPwd = await bcrypt.hash(this.password, 10);
+    this.password = hashedPwd;
+  }
+}
+
+export class Driver implements InferSelectModel<typeof users> {
+  id: string;
+  createdAt: Date;
+  username: string;
+  password: string;
+  role: string;
+
+  static fromDto(dto: RegisterDriverDto): User {
+    const user = new User();
+    user.id = nanoid();
+    user.createdAt = new Date();
+    user.username = dto.username;
+    user.password = dto.password;
+    user.role = 'driver';
 
     return user;
   }
