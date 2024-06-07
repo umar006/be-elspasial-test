dec1bd42f01270181b7cc9b6d5763bc483bf57a8
umar006 <umaralfaruq597@gmail.com>
2024-06-07 14:16:10 +0700

    feat: login user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { CreateUserDto } from './create-user.dto';
+import { LoginDto } from './login.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +11,11 @@ export class UsersController {
   async create(@Body() createUserDto: CreateUserDto) {
     const resp = await this.usersService.create(createUserDto);
     return { message: resp };
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const resp = await this.usersService.login(loginDto);
+    return { token: resp };
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,18 +3,24 @@ import {
   Inject,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { eq } from 'drizzle-orm';
 import { PostgresError } from 'postgres';
 import {
   DRIZZLE_PROVIDER,
   DrizzlePostgres,
 } from 'src/database/drizzle.provider';
 import { CreateUserDto } from './create-user.dto';
+import { LoginDto } from './login.dto';
 import { User, users } from './users.schema';
 
 @Injectable()
 export class UsersService {
   constructor(
+    private readonly jwtService: JwtService,
     @Inject(DRIZZLE_PROVIDER)
     private readonly db: DrizzlePostgres,
   ) {}
@@ -36,5 +42,27 @@ export class UsersService {
     }
 
     return 'success create user';
+  }
+
+  async login(loginDto: LoginDto) {
+    const [user] = await this.db
+      .select()
+      .from(users)
+      .where(eq(users.username, loginDto.username));
+    if (!user) throw new UnauthorizedException('username or password is wrong');
+
+    const comparePwd = await bcrypt.compare(loginDto.password, user.password);
+    if (!comparePwd)
+      throw new UnauthorizedException('username or password is wrong');
+
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      role: user.role,
+    };
+
+    const token = await this.jwtService.signAsync(payload);
+
+    return token;
   }
 }
