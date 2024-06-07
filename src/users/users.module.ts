f4a158d2ea45b736097e3552dc8f5ee9f3c7df1f
umar006 <umaralfaruq597@gmail.com>
2024-06-07 14:10:28 +0700

    feat: setup jwt module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { ConfigType } from '@nestjs/config';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import authConfig from './auth.config';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [authConfig.KEY],
+      useFactory: (
+        authCfg: ConfigType<typeof authConfig>,
+      ): JwtModuleOptions => {
+        const jwtSecret = authCfg.JWT_SECRET;
+        let jwtExpiresInHour = parseInt(authCfg.JWT_EXPIRES_IN_HOUR);
+        if (isNaN(jwtExpiresInHour)) {
+          jwtExpiresInHour = 1;
+        }
+
+        return {
+          secret: jwtSecret,
+          signOptions: {
+            expiresIn: `${jwtExpiresInHour}h`,
+          },
+        };
+      },
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
