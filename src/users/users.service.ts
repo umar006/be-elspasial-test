5a0a6ed91a9002724fe19795d0d9416232addca5
umar006 <umaralfaruq597@gmail.com>
2024-06-07 13:15:03 +0700

    feat: handler username already exists

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PostgresError } from 'postgres';
 import {
   DRIZZLE_PROVIDER,
   DrizzlePostgres,
@@ -16,7 +22,18 @@ export class UsersService {
   async create(createUserDto: CreateUserDto) {
     const user = User.fromDto(createUserDto);
     await user.encryptPassword();
-    await this.db.insert(users).values(user);
+
+    try {
+      await this.db.insert(users).values(user);
+    } catch (err) {
+      console.error(err);
+      if (err instanceof PostgresError) {
+        if (err.code === '23505') {
+          throw new ConflictException('username already exists');
+        }
+      }
+      throw new InternalServerErrorException();
+    }
 
     return 'success create user';
   }
