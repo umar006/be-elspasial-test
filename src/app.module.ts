0726debd73c13868131c100285ce27419a003584
umar006 <umaralfaruq597@gmail.com>
2024-06-10 21:37:28 +0700

    feat: setup rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule, seconds } from '@nestjs/throttler';
 import authConfig from './auth/auth.config';
+import { AuthModule } from './auth/auth.module';
 import databaseConfig from './database/database.config';
 import { DatabaseModule } from './database/database.module';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,12 +15,23 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       load: [databaseConfig, authConfig],
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: seconds(60),
+        limit: 100,
+      },
+    ]),
     DatabaseModule,
     UsersModule,
     OrdersModule,
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
