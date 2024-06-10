3f1e8fcbac827a806310370aef80fd802a21cefa
umar006 <umaralfaruq597@gmail.com>
2024-06-10 13:10:47 +0700

    feat: define api response with example register user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,12 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiExtraModels,
+  ApiInternalServerErrorResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { LoginDto } from './login-user.dto';
 import { RegisterUserDto } from './register-user.dto';
 import { User } from './users.schema';
@@ -12,6 +19,43 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('register')
+  @ApiCreatedResponse({
+    description: 'success register user',
+    schema: {
+      example: {
+        message: 'success create user',
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'validate request body',
+    schema: {
+      example: {
+        message: ['username should not be empty'],
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+    },
+  })
+  @ApiConflictResponse({
+    description: 'username already exists',
+    schema: {
+      example: {
+        message: 'username already exists',
+        error: 'Conflict',
+        statusCode: 409,
+      },
+    },
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'something went wrong',
+    schema: {
+      example: {
+        message: 'Internal Server Error',
+        statusCode: 500,
+      },
+    },
+  })
   async register(
     @Body() registerDto: RegisterUserDto,
   ): Promise<{ message: string }> {
