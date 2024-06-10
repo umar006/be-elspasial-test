e8fced0d7c57a4b34e7c47922720e57f42235c07
umar006 <umaralfaruq597@gmail.com>
2024-06-10 10:01:21 +0700

    feat: put role to each api

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from 'src/auth/jwt.guard';
+import { Roles } from 'src/auth/role.decorator';
+import { Role } from 'src/auth/role.enum';
 import { CreateOrderDto } from './create-order.dto';
 import { OrderQueryParams } from './order.param';
 import { OrderResponse } from './order.schema';
 import { OrdersService } from './orders.service';
 
+@UseGuards(JwtGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Roles(Role.User)
   @Post()
   async createOrder(
     @Body() createOrderDto: CreateOrderDto,
@@ -16,12 +30,14 @@ export class OrdersController {
     return { message: resp };
   }
 
+  @Roles(Role.User)
   @Get(':id')
   async getOrderById(@Param('id') orderId: string): Promise<OrderResponse> {
     const resp = await this.ordersService.getOrderById(orderId);
     return resp;
   }
 
+  @Roles(Role.Driver)
   @Get()
   async getOrders(
     @Query() queryParams: OrderQueryParams,
@@ -30,6 +46,7 @@ export class OrdersController {
     return resp;
   }
 
+  @Roles(Role.Driver)
   @Put(':id/accept')
   async acceptOrder(@Param('id') id: string): Promise<{ message: string }> {
     const resp = await this.ordersService.acceptOrderById(id);
