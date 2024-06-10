d193280b73938c45b5ced79ccb48c74e080a050e
umar006 <umaralfaruq597@gmail.com>
2024-06-10 13:32:47 +0700

    feat: define api response with example for login user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,7 @@ import {
   ApiCreatedResponse,
   ApiExtraModels,
   ApiInternalServerErrorResponse,
+  ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { LoginDto } from './login-user.dto';
@@ -28,7 +29,7 @@ export class UsersController {
     },
   })
   @ApiBadRequestResponse({
-    description: 'validate request body',
+    description: 'validation error',
     schema: {
       example: {
         message: ['username should not be empty'],
@@ -64,6 +65,33 @@ export class UsersController {
   }
 
   @Post('login')
+  @ApiOkResponse({
+    description: 'success login user',
+    schema: {
+      example: {
+        token: 'longjwttoken',
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'validation error',
+    schema: {
+      example: {
+        message: ['username should not be empty'],
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+    },
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'something went wrong',
+    schema: {
+      example: {
+        message: 'Internal Server Error',
+        statusCode: 500,
+      },
+    },
+  })
   @HttpCode(HttpStatus.OK)
   async login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
     const resp = await this.usersService.login(loginDto);
