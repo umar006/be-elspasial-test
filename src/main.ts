9fbbfb5ba69cde8db0cadd0b6349fc9bfe2a1096
umar006 <umaralfaruq597@gmail.com>
2024-06-11 07:33:17 +0700

    modify: path api doc

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-doc', app, document);
 
   await app.listen(3000);
 }
